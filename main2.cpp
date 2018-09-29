d1ca2de01fb2baaa25041d3fde1543e95f821b07
John Murdock <jmurdockdev@gmail.com>
2018-09-28 20:35:30 -0400

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -5,7 +5,8 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <fstream>
-
+//#include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -15,25 +16,46 @@ char line[MAXLINE];
 int main(int argc, char *argv[])
 {
 
-    string arr[10];
+    //string arr[10];
+    // email will be argv[1]
+
+
 
     FILE *output = popen("find . -name \"*.c\"" , "w");
 
     int i = 0;
     while (fgets(line, MAXLINE, output) != NULL)
     {
-    //copy line into the array;
-        puts (line);
-    i++;
+        //cout << i << endl;
+        //copy line into the array;
+
+        i++;
     }
 
-    //pclose(output);
 
-    for(int j = 0 ; j < 5 ; j++)
-    {
-        cout <<  line << endl;
+    pclose(output);
 
-    }
+
+    string num;
+    stringstream convert;
+    convert << i ;
+    num = convert.str();
+
+    stringstream ss("find . -name \"*.c\" | mailx -s ", ios_base::app | ios_base::out);
+
+    ss << "\"" << num << "\" " << argv[1];
+
+    string str(ss.str());
+
+    const char* command = str.c_str();
+
+
+
+    cout << command << endl;
+
+    popen(command , "w" );
+
+
 
     return 0;
 }
